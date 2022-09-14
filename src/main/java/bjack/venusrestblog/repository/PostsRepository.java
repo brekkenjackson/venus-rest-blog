1b80e3deca4d00fd9fa7f56bf57e874ff2ccec20
brekkenjackson <106278680+brekkenjackson@users.noreply.github.com>
2022-09-14 13:44:27 -0500

    Posts repository

---
+++ src/main/java/bjack/venusrestblog/repository/PostsRepository.java
@@ -0,0 +1,9 @@
+package bjack.venusrestblog.repository;
+
+
+import bjack.venusrestblog.data.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Post, Long> {
+
+}
